63dff32eda58cd208bee53456874fb89bda5d0a1
yujaemin <woawoa0513@gmail.com>
2023-01-15 17:45:54 +0900

    feat: Posts 수정,조회 컨트롤러 생성

--- src/main/java/org/example/springboot/web/PostsApiController.java
+++ src/main/java/org/example/springboot/web/PostsApiController.java
@@ -2,8 +2,13 @@ package org.example.springboot.web;
 
 import lombok.RequiredArgsConstructor;
 import org.example.springboot.service.posts.PostsService;
+import org.example.springboot.web.dto.PostsResponseDto;
 import org.example.springboot.web.dto.PostsSaveRequestDto;
+import org.example.springboot.web.dto.PostsUpdateRequestDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,4 +23,13 @@ public class PostsApiController {
         return postsService.save(requestDto);
     }
 
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
 }
