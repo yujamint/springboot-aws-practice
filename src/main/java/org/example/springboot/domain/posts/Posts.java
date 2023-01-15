46f17e49c3a93394172be1811baa6a7c653a6bcc
yujaemin <woawoa0513@gmail.com>
2023-01-16 01:19:53 +0900

    feat: BaseTimeEntity 클래스 상속

--- src/main/java/org/example/springboot/domain/posts/Posts.java
+++ src/main/java/org/example/springboot/domain/posts/Posts.java
@@ -8,11 +8,12 @@ import javax.persistence.Id;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.example.springboot.domain.BaseTimeEntity;
 
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
